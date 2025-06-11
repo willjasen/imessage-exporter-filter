77a64ddceebfdfd4da9f15f82ef270c05a59201e
willjasen <brandon@willjasen.com>
2025-06-11 13:45:22 -0400

    use --conversation-filter

--- messages.sh
+++ messages.sh
@@ -15,9 +15,12 @@ while IFS="," read -r number start_date end_date
 do
   echo -e "\n\n\nCreating HTML to $number from $start_date to $end_date\n\n\n"
   mkdir "$PWD/$start_date to $end_date"
-  $BIN -l -c clone -f html -o "$PWD/$start_date to $end_date" -s $start_date -e $end_date
+
+  $BIN -l -c clone -f html -o "$PWD/$start_date to $end_date" -s $start_date -e $end_date \
+    --conversation-filter $number;
+  
   #sleep 5
-  find "$PWD/$start_date to $end_date" -type f -maxdepth 1 -not -name "$number.html" -delete
+  # find "$PWD/$start_date to $end_date" -type f -maxdepth 1 -not -name "$number.html" -delete
 
   # Convert HTML to PDF
   # wkhtmltopdf --enable-local-file-access "file://$PWD/$start_date to $end_date/$number.html" "$PWD/$start_d$
