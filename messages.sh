13adf639e2371255b95e87842c96979b543c98f7
willjasen <brandon@willjasen.com>
2025-06-11 13:37:05 -0400

    change -c option to clone

--- messages.sh
+++ messages.sh
@@ -15,7 +15,7 @@ while IFS="," read -r number start_date end_date
 do
   echo -e "\n\n\nCreating HTML to $number from $start_date to $end_date\n\n\n"
   mkdir "$PWD/$start_date to $end_date"
-  $BIN -l -c compatible -f html -o "$PWD/$start_date to $end_date" -s $start_date -e $end_date
+  $BIN -l -c clone -f html -o "$PWD/$start_date to $end_date" -s $start_date -e $end_date
   #sleep 5
   find "$PWD/$start_date to $end_date" -type f -maxdepth 1 -not -name "$number.html" -delete
 
