7d20c3d880aa74417fe7b0d55dbba7eadc3a5862
willjasen <brandon@willjasen.com>
2025-06-11 14:02:27 -0400

    output as TXT as well

--- messages.sh
+++ messages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BIN="/Users/willjasen/.cargo/bin/imessage-exporter"
+OUTPUT_DIR="$PWD/exported_messages"
 
 # Check if imessage-exporter is installed
 if [ ! -f $BIN ]; then
@@ -14,12 +15,23 @@ fi
 while IFS="," read -r number start_date end_date
 do
   echo -e "\n\n\nCreating HTML for $number from $start_date to $end_date\n\n\n"
-  mkdir -p "$PWD/$number"
+  mkdir -p "$OUTPUT_DIR/HTML/$number"
 
-  $BIN -l -c clone -f html -o "$PWD/$number" -s $start_date -e $end_date \
+  $BIN -l -c clone -f html -o "$OUTPUT_DIR/HTML/$number" -s $start_date -e $end_date \
+    --conversation-filter $number;
+
+  # Export as TXT
+  echo -e "\n\n\nExporting messages for $number to TXT\n\n\n"
+  mkdir -p "$OUTPUT_DIR/TXT/$number"
+  $BIN \
+    --no-lazy \
+    --format txt \
+    --export-path "$OUTPUT_DIR/TXT/$number" \
+    --copy-method clone \
     --conversation-filter $number;
 
   # Convert HTML to PDF
   # wkhtmltopdf --enable-local-file-access "file://$PWD/$start_date to $end_date/$number.html" "$PWD/$start_d$
 
 done < messages.csv
+
